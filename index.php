5512b31eb23803f0b51b8bd62e73c1c8fe794bf1
EVM <elyzeo.vm@gmail.com>
2019-12-02 19:28:25 -0600

    Creando el Formulario

--- index.php
+++ index.php
@@ -4,6 +4,31 @@
     <h1>Agenda de Contactos</h1>
 </div>
 
+<div class="bg-amarillo contenedor sombra">
+    <form action="#" id="contacto">
+        <legend>Añada un contacto <span>Todos los campos son obligatorios</span> </legend>
+        <div class="campos">
+            <div class="campo">
+                <label for="nombre">Nombre:</label>
+                <input type="text" placeholder="Nombre Contacto" id="nombre">
+            </div>
+
+            <div class="campo">
+                <label for="empresa">Empresa:</label>
+                <input type="text" placeholder="Nombre Empresa" id="empresa">
+            </div>
+
+            <div class="campo">
+                <label for="telefono">Teléfono:</label>
+                <input type="tel" placeholder="Numero Telefonico" id="telefono">
+            </div>
+            <div class="campo-enviar">
+                <input type="submit" value="Añadir">
+            </div>
+        </div>
+    </form>
+</div>
+
 
 
 <?php include 'inc/layout/footer.php'; ?>
