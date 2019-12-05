e6bc1d13799dd576b87d6e4f0831b860afabaa19
EVM <elyzeo.vm@gmail.com>
2019-12-05 09:49:33 -0600

    Realizando una peticion con Ajax

--- js/app.js
+++ js/app.js
@@ -13,14 +13,50 @@ function leerFormulario(e) {
     // Leer los datos de los inputs
     const nombre = document.querySelector('#nombre').value,
         empresa = document.querySelector('#empresa').value,
-        telefono = document.querySelector('#telefono').value;
+        telefono = document.querySelector('#telefono').value,
+        accion = document.querySelector('#accion').value;
 
     if (nombre === '' || empresa === '' || telefono === '') {
         // 2 parametros: texto y clase
         mostrarNotificacion('Todos los campos son obligatorios', 'error');
     } else {
-        console.log('Tiene algo');
+        // Pasa a la validación, crear llamado a Ajax
+        const infoContacto = new FormData();
+        infoContacto.append('nombre', nombre);
+        infoContacto.append('empresa', empresa);
+        infoContacto.append('telefono', telefono);
+        infoContacto.append('accion', accion);
+
+        console.log(...infoContacto);
+
+        if (accion === 'crear') {
+            // Creamos un nuevo elemento
+            insertarBD(infoContacto);
+        } else {
+            // Editar el contacto
+
+        }
     }
+}
+
+// Insertar en la base de datos via Ajax
+function insertarBD(datos) {
+    // Llamando a ajax
+    // Ceear el objeto
+    const xhr = new XMLHttpRequest();
+    // Abrir la conexion
+    xhr.open('POST', 'inc/modelos/modelo-contactos.php', true);
+    // Pasar los datos
+    xhr.onload = function() {
+            if (this.status === 200) {
+                console.log(JSON.parse(xhr.responseText));
+                // Leemos la respuesta en PHP
+                const respuesta = JSON.parse(xhr.responseText);
+                console.log(respuesta.empresa);
+            }
+        }
+        // Enviar los datos
+    xhr.send(datos);
 }
 
 // Notificacion en Pantalla
